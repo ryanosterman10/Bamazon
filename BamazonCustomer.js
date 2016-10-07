c2978b965e049c1618acc5063cb5f1d6f7594ba2
Ryan Osterman <ryanosterman10@Ryans-Air.byod.sas.com>
2016-10-07 14:08:19 -0400

    connection created

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,1 +1,19 @@
+var mysql = require('mysql');
+var prompt = require('prompt');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'BamazonDB'
+});
+
+connection.connect(function(err){
+	if(err){
+		throw err;
+	}
+	console.log("connected as id", connection.threadId);
+});
+
 console.log("Bamazon!");
